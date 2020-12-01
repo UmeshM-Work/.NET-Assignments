d555ad60b98fe554c387c49f7999a01d2f7ccf06
UMESH WORK <65343874+UmeshM-Work@users.noreply.github.com>
2020-12-02 01:13:50 +0530

    Add files via upload

---
+++ Day2/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Employee o1 = new Employee("Amol", 123465, 10);
+            Console.WriteLine("Employee id : "+ " "+ o1.EMPNO);
+            Console.WriteLine("Employee name : " + " " + o1.NAME);
+            Console.WriteLine("Employee Basic Salary : " + " " + o1.BASIC);
+            Console.WriteLine("Employee DEPTNO : " + " " + o1.DEPTNO);
+            Console.WriteLine(" ");
+
+
+            Employee o2 = new Employee("Amol", 123465);
+            Console.WriteLine("Employee id : " + " " + o2.EMPNO);
+            Console.WriteLine("Employee name : " + " " + o2.NAME);
+            Console.WriteLine("Employee Basic Salary : " + " " + o2.BASIC);
+            Console.WriteLine(" ");
+
+            Employee o3 = new Employee("Amol");
+            Console.WriteLine("Employee id : " + " " + o3.EMPNO);
+            Console.WriteLine("Employee name : " + " " + o3.NAME);
+            Console.WriteLine(" ");
+
+            Employee o4 = new Employee();
+            Console.WriteLine("Employee id : " + " " + o4.EMPNO);
+
+
+            #region Testcases
+            //Employee o1 = new Employee();
+            //Employee o2 = new Employee();
+            //Employee o3 = new Employee();
+
+            //Console.WriteLine(o1.EMPNO);
+            //Console.WriteLine(o2.EMPNO);
+            //Console.WriteLine(o3.EMPNO);
+
+
+            #endregion
+
+            Console.ReadLine();
+
+        }
+    }
+
+   class Employee
+    {
+
+        private String name;
+        public static int Empno;
+        private Decimal Basic;
+        private short DeptNo;
+
+
+        #region property
+
+        #region Name
+        public String NAME
+        {
+
+
+            set { 
+                if(value !=" ")
+                {
+                    name = value;
+                }
+                else
+                {
+
+                    Console.WriteLine("Please enter Name dont leave it blank");
+                }
+            }
+
+            get {
+
+                return name;
+            }
+
+        }
+
+        #endregion
+
+
+        #region EMPNO
+        public int EMPNO
+        {
+
+            get
+            {
+
+                return Empno;
+            }
+
+        }
+        #endregion
+
+        #region Basic
+        public Decimal BASIC
+        {
+            set
+            {
+                if (value > 100000 && value < 1000000)
+                {
+                    Basic = value;
+                }
+                else
+                {
+                    Console.WriteLine("Salary out of range please enter once again !!");
+                }
+            }
+
+            get
+            {
+
+                return Basic;
+            }
+
+        }
+        #endregion
+
+
+        #region DEPTNO
+        public short DEPTNO
+        {
+            set { 
+            
+                if(value>0)
+                {
+                    DeptNo = value;
+                }
+
+                else
+                {
+                    Console.WriteLine("Deptno should be greater than zero please enter once again !!");
+                }
+
+
+            }
+
+            get {
+
+                return DeptNo; 
+           
+            }
+        }
+
+        #endregion
+
+
+
+        #region GETSALARY
+        public decimal GetNetSalary()
+        {
+
+            return 12 * Basic;
+
+        }
+        #endregion
+
+        #endregion
+
+
+        #region  Contructor and param- Constructor
+
+        public Employee(String n,Decimal b,short d)
+        {
+            this.NAME = n;
+            Empno++;
+            this.Basic = b;
+            this.DEPTNO = d;
+        }
+
+        public Employee(String n, Decimal b)
+        {
+            this.NAME = n;
+
+            Empno++;
+            this.Basic = b;
+        }
+
+        public Employee(String n)
+        {
+            this.NAME = n;
+            Empno++;
+        }
+
+        public Employee()
+        {
+
+            Empno++;
+                
+        }
+
+        #endregion
+
+
+
+
+    }
+
+
+}
